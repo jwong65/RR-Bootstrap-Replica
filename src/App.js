5715f1cd88b3dea84d76ffc0986bf3df57b4460e
jwong65 <jason.wong6507@gmail.com>
2023-02-02 18:37:21 -0500

    Added the NavBar.js and added the imports from the react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavigationBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Hello World</h1>
     </div>
   );
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { NavDropdown } from 'react-bootstrap/NavDropdown'
+
+export default function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}
